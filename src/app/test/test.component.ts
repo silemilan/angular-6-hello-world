9a0695cea9c13f5f4808b1c61b050f12d1f8b3d0
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2018-07-03 16:57:27 +0100

    ended

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,24 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>Welcome {{name}}</h2>
-    <input #myInput type="text">
-    <button (click)="logMessage(myInput.value)">Log</button>
-    <h3>{{valueToDisplay}}</h3>
+    <input [(ngModel)]="name" type="text">
+    <h3>{{name}}</h3>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public name = 'Dariusz';
-  public valueToDisplay = '';
+  public name = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  logMessage(value) {
-    this.valueToDisplay = value;
-  }
+
 }
