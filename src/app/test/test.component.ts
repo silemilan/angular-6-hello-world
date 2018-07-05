6775f617cb0c5a369cbe7aa69491f12603a673ea
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2018-07-05 12:16:21 +0100

    completed

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,14 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <input [(ngModel)]="name" type="text">
-    <h3>{{name}}</h3>
+    <h2 *ngIf="true"> Text displayed by ngIf Directive </h2>
+    <h2 *ngIf="false"> Text hidden by ngIf Directive </h2>
+    <h2 *ngIf="displayName"> Text displayed by ngIf Directive </h2>
+
+    <h2 *ngIf="displayName2; else elseBlock"> Text hidden by ngIf Directive </h2>
+    <ng-template #elseBlock>
+      <h2> Another text displayed by ngIf Directive with else statement</h2>
+    </ng-template>
+
+
+    <h2 *ngIf="displayName3; then thenBlock; else elseBlock2"></h2>
+
+    <ng-template #thenBlock>
+      <h2> Another text displayed by ngIf Directive with then/else statements</h2>
+    </ng-template>
+
+    <ng-template #elseBlock2>
+      <h2> Text hidden by ngIf Directive</h2>
+    </ng-template>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public name = '';
+  displayName = true;
+  displayName2 = false;
+  displayName3 = true;
 
   constructor() { }
 
